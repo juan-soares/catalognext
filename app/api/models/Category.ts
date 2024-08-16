e0591bc4b66975a796a36a81762125f7849bee96
juan-soares <juan.css013@gmail.com>
2024-08-16 07:30:14 -0300

    A

--- app/api/models/Category.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-const categorySchema = new Schema({
-  collectionRef: { type: String, required: true },
-});
-
-const Category = mongoose.model("Category", categorySchema);
-
-export default Category;
