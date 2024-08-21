9e6e2f26136e4ff85a36b614cf922201f984c984
juan-soares <juan.css013@gmail.com>
2024-08-21 00:31:16 -0300

    as

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,11 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getUser } from "@/_services/user";
+import { ICredentials } from "@/_interfaces";
 
 export async function POST(req: NextRequest) {
-  const reqCredentials = await req.json();
+  const { email, password }: ICredentials = await req.json();
+
+  if (!email || !password) {
+    throw new Error("Credenciais não fornecidas.");
+  }
 
   try {
-    const user = await getUser(reqCredentials);
+    const user = await getUser({ email, password });
     return NextResponse.json(user);
   } catch (error) {
     console.error("Ops! Erro no arquivo route.ts:" + error);
